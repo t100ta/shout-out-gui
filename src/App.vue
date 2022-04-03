60ebee572df0386991f07bb238fb10601b9123e5
t100ta <cjx.open_d@me.com>
2022-04-03 19:07:17 +0900

    bot設定登録ページ作成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/settings">About</router-link> |
     <router-link to="comment">Create Comment</router-link>
   </nav>
   <router-view />
---
+++ src/views/BotSettingsView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="bot-settings">
+    <h2>OAUTH TOKEN</h2>
+    <input v-model="oauthToken" placeholder="oauth:..." />
+    <p>{{ oauthToken }}</p>
+    <open-outline width="8" height="8" fillColor="red" strokeColor="red" />
+    <open-outline width="32" height="32" strokeColor="black" />
+
+    <h2>CLIENT ID</h2>
+    <input v-model="clientId" placeholder="XXX" />
+    <p>{{ clientId }}</p>
+
+    <h2>S.O.通知botとして使うアカウントのユーザー名</h2>
+    <input v-model="botUsername" placeholder="your_bot" />
+    <p>{{ botUsername }}</p>
+
+    <h2>投稿先チャンネルとなるアカウントのユーザーID</h2>
+    <input v-model="channelID" placeholder="your_channel" />
+    <p>{{ channelID }}</p>
+
+    <h2>疎通チェック用カスタムコマンド</h2>
+    <input v-model="pingCommand" placeholder="!sobot" />
+    <p>{{ pingCommand }}</p>
+  </div>
+</template>
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import OpenOutline from "@/assets/openOutline.vue";
+
+@Options({
+  components: {
+    OpenOutline,
+  },
+})
+export default class BotSettingsView extends Vue {
+  oauthToken = "";
+  clientId = "";
+  botUsername = "";
+  channelID = "";
+  pingCommand = "";
+}
+</script>
