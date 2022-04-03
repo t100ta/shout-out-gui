46e735dfb98ad87e25eb0b2b2a2022b744fc728c
t100ta <cjx.open_d@me.com>
2022-04-03 19:06:27 +0900

    コメント編集ページ作成

--- src/views/CommentCreatorView.vue
+++ src/views/CommentCreatorView.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="comment-creater">
+    <span>見え方</span>
     <p>{{ replaceUserInfoParam }}</p>
-    <input v-model="comment" placeholder="edit me" />
+    <div class="button-box">
+      <button v-on:click="concatUtil('$displayname')">表示名</button>
+      <button v-on:click="concatUtil('$loginname')">ID</button>
+      <button v-on:click="concatUtil('$category')">カテゴリ</button>
+      <button v-on:click="concatUtil('$title')">タイトル</button>
+    </div>
+    <input v-model="comment" placeholder="ここに書く" />
+    <button>テンプレを保存する</button>
   </div>
 </template>
 <script lang="ts">
@@ -18,15 +26,34 @@ export default class CommentCreatorView extends Vue {
       $displayname: "にかなとむ",
       $loginname: "tom_t100ta",
       $category: "Silent Hill2",
-      $title: "第２回 全日本静岡マラソン #3",
+      $title: "第２回 全日本静岡マラソン大会 #3",
     };
 
     let replaced = this.comment;
     Object.keys(sampleUser).forEach((key) => {
       replaced = replaced.replaceAll(key, sampleUser[key]);
-      console.log(replaced);
     });
     return replaced;
   }
+
+  concatUtil(value: string) {
+    this.comment += `${value}`;
+  }
 }
 </script>
+<style lang="scss" scoped>
+.button-box {
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: space-around;
+  width: 80%;
+
+  button {
+    width: 5rem;
+  }
+}
+
+input {
+  width: 80%;
+}
+</style>
